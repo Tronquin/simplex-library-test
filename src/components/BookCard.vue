5c74ccd2c3e2eaf9c1c75328e59c87390d35423f
Francisco <tronquinwantsmoney@gmail.com>
2021-09-22 23:35:50 -0400

    Book Card finished, about route started

--- src/components/BookCard.vue
+++ src/components/BookCard.vue
@@ -1,7 +1,41 @@
 <template>
-  <v-card :book="book">
-    {{ book.title }}
-  </v-card>
+  <router-link
+    :to="{ name: 'About', params: { key: book.key, book: book } }"
+    class="text-decoration-none"
+  >
+    <v-card :book="book" class="book-card pa-4 mb-5">
+      <v-list-item three-line class="pa-0">
+        <v-img
+          :lazy-src="bookCover(book)"
+          max-height="200"
+          max-width="150"
+          :src="bookCover(book)"
+          class="mr-6"
+        ></v-img>
+        <v-list-item-content class="align-self-center pa-0">
+          <div class="text-h5 font-weight-bold mb-2">
+            {{ book.title }}
+          </div>
+          <div class="d-flex mb-4">
+            <v-list-item-title
+              class="font-weight-regular text-h6 mb-1 mr-2"
+              v-for="(author, i) in book.author_name"
+              :key="i"
+            >
+              {{ author }}{{ i !== book.author_name.length - 1 ? ", " : "" }}
+            </v-list-item-title>
+          </div>
+          <v-list-item-subtitle class="mb-2"
+            >First Published In
+            {{ book.first_publish_year }}</v-list-item-subtitle
+          >
+          <v-list-item-subtitle
+            >Editions: {{ book.edition_count }}</v-list-item-subtitle
+          >
+        </v-list-item-content>
+      </v-list-item>
+    </v-card>
+  </router-link>
 </template>
 
 <script>
@@ -13,7 +47,18 @@ export default {
   data() {
     return {};
   },
+  methods: {
+    bookCover(book) {
+      return `http://covers.openlibrary.org/b/isbn/${
+        book.isbn && book.isbn.length >= 0 ? book.isbn[0] : 0
+      }-M.jpg`;
+    },
+  },
 };
 </script>
 
-<style></style>
+<style>
+.book-card .v-list-item__title {
+  flex: 0 1 auto;
+}
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    {{ book }}
   </div>
 </template>
+
+<script>
+export default {
+  name: "about",
+  data() {
+    return { book: {} };
+  },
+  created() {
+    this.book = this.$route.params.book;
+  },
+};
+</script>
+
+<style></style>
